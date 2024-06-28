54f09872f2203bdf6cf3683954b7d5144c10a673
Gabriel Fosse <cryptogaboman@gmail.com>
2024-06-28 14:32:24 -0700

    stop on enter

--- pdms_tester.py
+++ pdms_tester.py
@@ -1,3 +1,11 @@
+
+"""
+This test will run until the user presses ENTER
+or the max duration (test_time) is reached. 
+
+Will write CSV file with 'timestamp' and 'voltage' values
+"""
+
 import pyvisa as visa
 import time
 import matplotlib.pyplot as plt
@@ -5,12 +13,13 @@ import pandas as pd
 from collections import deque
 from datetime import datetime
 import signal
+import threading
 
 # Import setup_instrument from your module
 from beta import setup_instrument
-
-# Duration of the test
-test_time = 3600  # 1 hour
+# Max Duration of the test
+# test_time = 3600  # 1 hour
+test_time = 9800  # 3 hour
 # test_time = 7200  # 2 hour
 
 print(f"running test for {test_time} seconds...")
@@ -64,9 +73,21 @@ def handle_exit(signum, frame):
 signal.signal(signal.SIGINT, handle_exit)
 signal.signal(signal.SIGTERM, handle_exit)
 
+# Flag to stop the main loop
+stop_flag = threading.Event()
+
+def wait_for_user_input():
+    """Wait for user input to stop the test."""
+    input("Press Enter to stop the test...\n")
+    stop_flag.set()
+
+# Start the thread to wait for user input
+input_thread = threading.Thread(target=wait_for_user_input)
+input_thread.start()
+
 try:
     # Main loop for continuous data acquisition
-    while time.time() - start_time < test_time:
+    while not stop_flag.is_set() and time.time() - start_time < test_time:
         # Query the instrument for DC voltage measurement
         volts = float(dmm.volt())
 
